51c569d6efadebfbb821442040f377a15a4e7204
Craig Petersen <craig@2deviate.com>
2024-05-01 19:17:40 +0100

    Removed test files. Changed configuration

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use utils::write_image;
 use complex:: {
     parse_pair,
     parse_complex,
-    render
+    render_async
 };
 
 
@@ -13,21 +13,37 @@ fn main() {
     use std::env;
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 5 {
-        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
-        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35", args[0]);
+    if args.len() < 5 {
+        eprintln!("Usage: {} FILE THREADS PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 64 1000x750 -1.20,0.35 -1,0.20", args[0]);
         std::process::exit(1);
     }
-    let bounds = parse_pair(&args[2], 'x')
+    let arg = match args.get(2) {
+        Some(val) => val,
+        None => {
+            println!("invalid argument supplied!");
+            return;
+        }
+    };
+
+    let threads = match arg.parse::<usize>(){
+        Ok(val) => val,
+        Err(e) => {
+            eprintln!("unable to parse number of threads from argument {}", e);
+            return;
+        }
+    };
+
+    let bounds = parse_pair(&args[3], 'x')
         .expect("error parsing image dimensions");
-    let upper_left = parse_complex(&args[3])
+    let upper_left = parse_complex(&args[4])
         .expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[4])
+    let lower_right = parse_complex(&args[5])
         .expect("error parsing lower right corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    render_async(threads, &mut pixels, bounds, upper_left, lower_right);
 
     write_image(&args[1], &pixels, bounds)
         .expect("error writing file");
