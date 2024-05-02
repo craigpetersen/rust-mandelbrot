91ab7f86c8ee3dde72068e4ddd4cc4750b16857c
Craig Petersen <craig@2deviate.com>
2024-05-02 09:31:21 +0100

    Added cpu flag and render timing

--- src/main.rs
+++ src/main.rs
@@ -7,46 +7,37 @@ use complex:: {
     parse_complex,
     render_async
 };
+use std::time::{Instant};
 
 
 fn main() {
     use std::env;
+    let cpus = num_cpus::get();
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 5 {
-        eprintln!("Usage: {} FILE THREADS PIXELS UPPERLEFT LOWERRIGHT", args[0]);
-        eprintln!("Example: {} mandel.png 64 1000x750 -1.20,0.35 -1,0.20", args[0]);
+    if args.len() < 4 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20", args[0]);
         std::process::exit(1);
     }
-    let arg = match args.get(2) {
-        Some(val) => val,
-        None => {
-            println!("invalid argument supplied!");
-            return;
-        }
-    };
 
-    let threads = match arg.parse::<usize>(){
-        Ok(val) => val,
-        Err(e) => {
-            eprintln!("unable to parse number of threads from argument {}", e);
-            return;
-        }
-    };
-
-    let bounds = parse_pair(&args[3], 'x')
+    let bounds = parse_pair(&args[2], 'x')
         .expect("error parsing image dimensions");
-    let upper_left = parse_complex(&args[4])
+    let upper_left = parse_complex(&args[3])
         .expect("error parsing upper left corner point");
-    let lower_right = parse_complex(&args[5])
+    let lower_right = parse_complex(&args[4])
         .expect("error parsing lower right corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render_async(threads, &mut pixels, bounds, upper_left, lower_right);
+    let start = Instant::now();
+
+    render_async(cpus, &mut pixels, bounds, upper_left, lower_right);
+
+    let duration = start.elapsed();
 
     write_image(&args[1], &pixels, bounds)
         .expect("error writing file");
 
-    println!("Done!");
+    println!("Done! Render time on {} cpus ~ {} ms", cpus, duration.as_millis().to_string());
 }
